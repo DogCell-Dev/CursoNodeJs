e13fdf0c9f6121af818ae98c95eed0c9a436f490
Dogcell-Dev <pitermirandamacedopc1@gmail.com>
2026-09-25 01:25:17 -0300

    Concluindo Curso ufaaaa😮‍💨

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const dotenv = require('dotenv')
 
 dotenv.config() 
 
-const connectToDatabase = require('./modules/connect.js')
+const connectToDatabase = require('./src/database/connect.js')
+require("./modules/express.js");
 
 connectToDatabase()
 
 
 
+
 //const person = new Person("Piter"); // Criando Novo objeto usando o Módulo
 //const { Person } = require("./person"); // Importa a classe/módulo Person
-//require("./modules/express.js");
 //require("./modules/http");
 // console.log(person.sayMyName());
 // require("./modules/path"); // Executa o módulo imediatamente
--- modules/express.js
+++ modules/express.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const UserModel = require("../src/models/user.model");
 const port = 8080;
-
 const app = express();
+
+//Use como view engine a biblioteca ejs
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.get('/views/users', async (req, res) => {
+  const users = await UserModel.find({})// Pega usuarios e coloca dessa variavel
+ 
+  res.render('index', { users })
+})
+
+
+
+//Sinalizando que vamos receber requisinções Post em formato JSON
+app.use(express.json());
+
+//midware next não deixa requisição terminar se não for chamado
+app.use((req, res, next) => {
+  console.log(`Request Type ${req.method}`);
+  console.log(`Content Type ${req.headers["Content-Type"]}`);
+  console.log(`Date ${new Date()}`);
+  
+  next()
+});
+
 app.get("/home", (req, res) => {
   res.contentType("application/html");
   res
@@ -11,7 +36,7 @@ app.get("/home", (req, res) => {
     );
 });
 
-app.get("/users", (req, res) => {
+/*app.get("/users", (req, res) => {
   const users = [
     {
       name: "Piter Miranda",
@@ -32,7 +57,61 @@ app.get("/users", (req, res) => {
   ];
   res.status(200).json(users);
 });
+*/
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
 
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// Definindo parâmetro com /:NomeParâmetro
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id; // Nesse exemplo é id mas se la na url o nome do parâmetro fosse nome, aqui seria req.params.nome
+
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+//Post
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log("error:", error);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 
 app.listen(port, () => console.log(`Rodando com express na porta ${port}`));
